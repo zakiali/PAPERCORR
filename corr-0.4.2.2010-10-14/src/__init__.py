39761d8fbec732db3d2198c66481e9c9b5697b22
Zaki Ali <zakiali88@gmail.com>
2012-06-11 13:36:32 -0700

    added bitfields to __init__.py

--- corr-0.4.2.2010-10-14/src/__init__.py
+++ corr-0.4.2.2010-10-14/src/__init__.py
@@ -14,4 +14,4 @@ ver0.4.2: 2010-04-02
     *) cleanup of some functions.
 
 """
-import cn_conf, sim_cn_data, katcp_wrapper, log_handlers, corr_functions
+import cn_conf, sim_cn_data, katcp_wrapper, log_handlers, corr_functions, bitfields
